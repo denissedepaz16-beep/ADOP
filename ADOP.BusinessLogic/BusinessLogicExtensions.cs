ed28e6a68a1cfdb31192425863f37c3d5c464a13
Andrea <denissedepaz16@gmail.com>
2026-05-14 01:21:17 -0600

    `feat: agregar extensiones de inyección de dependencias para BusinessLogic y DataAccess` - Andrea Denisse Ortiz De Paz

---
+++ ADOP.BusinessLogic/BusinessLogicExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ADOP.DataAccess;
+using Mapster;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ADOP.BusinessLogic
+{
+    public static class BusinessLogicExtensions
+    {
+        public static IServiceCollection AddBusinessLogicServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(
+                cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+            );
+
+            services.AddDataAccessServices(configuration);
+
+            TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
